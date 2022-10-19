128fec2cf426e093734ba4018bfd81b3d57c769f
RichardKanin <78396440+RichardKanin@users.noreply.github.com>
2022-10-18 21:07:27 -0400

    DynamoDB Writes

--- awesomeProject/logglyassignment.go
+++ awesomeProject/logglyassignment.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	_ "context"
 	"encoding/json"
+	_ "errors"
 	"fmt"
 	_ "fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/jamespearly/loggly"
 	"github.com/joho/godotenv"
 	"io"
@@ -13,9 +19,18 @@ import (
 	"net/http"
 	_ "net/http"
 	_ "os"
+	_ "strconv"
 )
 import _ "github.com/jamespearly/loggly"
 import _ "github.com/joho/godotenv"
+import _ "github.com/aws/aws-sdk-go/aws"
+import _ "github.com/aws/aws-sdk-go/aws/session"
+import _ "github.com/aws/aws-sdk-go/service/dynamodb"
+import _ "github.com/aws/aws-sdk-go/aws/credentials"
+import _ "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+import _ "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+import _ "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+import _ "github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 
 // type of JSON response struct
 
@@ -23,16 +38,16 @@ type malUser struct {
 	CurrentCondition []CurrentCondition `json:"CurrentCondition"`
 }
 type CurrentCondition struct {
-	LocalObservationDayTime string      `json:"LocalObservationDateTime"`
-	EpochTime               int         `json:"EpochTime"`
-	WeatherText             string      `json:"WeatherText"`
-	WeatherIcon             int         `json:"WeatherIcon"`
-	HasPrecipitation        bool        `json:"HasPrecipitation"`
-	PrecipitationType       *string     `json:"PrecipitationType"`
-	IsDayTime               bool        `json:"IsDayTime"`
-	Temperature             Temperature `json:"Temperature"`
-	MobileLink              string      `json:"MobileLink"`
-	Link                    string      `json:"Link"`
+	LocalObservationDateTime string      `json:"LocalObservationDateTime"`
+	EpochTime                int         `json:"EpochTime"`
+	WeatherText              string      `json:"WeatherText"`
+	WeatherIcon              int         `json:"WeatherIcon"`
+	HasPrecipitation         bool        `json:"HasPrecipitation"`
+	PrecipitationType        *string     `json:"PrecipitationType"`
+	IsDayTime                bool        `json:"IsDayTime"`
+	Temperature              Temperature `json:"Temperature"`
+	MobileLink               string      `json:"MobileLink"`
+	Link                     string      `json:"Link"`
 }
 
 type Temperature struct {
@@ -53,6 +68,7 @@ type Imperial struct {
 }
 
 func main() {
+
 	response, err := http.Get(`http://dataservice.accuweather.com/currentconditions/v1/329828?apikey=aqKf60jG6EIL9LqZKSf5KGnRAH1prGPe&language=en-us&details=false`)
 	if err != nil {
 		log.Fatal(err)
@@ -64,7 +80,8 @@ func main() {
 		log.Fatal(err1)
 	}
 
-	log.Print(response.Body)
+	//FIRST PRINT
+	//log.Print(response.Body)
 	bytes, errRead := io.ReadAll(response.Body)
 
 	defer func() {
@@ -77,7 +94,8 @@ func main() {
 	if errRead != nil {
 		log.Fatal(errRead)
 	}
-	log.Print(string(bytes))
+	//SECOND PRINT
+	//log.Print(string(bytes))
 
 	var currentCondition []CurrentCondition
 
@@ -86,7 +104,14 @@ func main() {
 		log.Fatal(errUnmarshal)
 	}
 
-	log.Printf("%+v", currentCondition)
+	b, err := json.MarshalIndent(currentCondition, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(string(b))
+
+	//THIRD PRINT
+	//log.Printf("%+v", b)
 
 	var tag string
 	tag = "LogglyAssignment"
@@ -98,4 +123,74 @@ func main() {
 	if err2 != nil {
 		log.Fatal(err2)
 	}
+
+	//time.Sleep(1 * time.Hour)
+
+	//DYNAMODB STUFF
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+
+		/*Config: aws.Config{
+			Region: aws.String("us-east-1"),
+		},*/
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	//dynamodb client creation
+	svc := dynamodb.New(sess)
+
+	// Create table Movies
+	tableName := "rkanin-accuweather"
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("LocalObservationDateTime"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("WeatherIcon"),
+				AttributeType: aws.String("N"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("LocalObservationDateTime"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("WeatherIcon"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.CreateTable(input)
+	if err != nil {
+		log.Fatalf("Got error calling CreateTable: %s", err)
+	}
+
+	//log.Print("Created the table", tableName)
+
+	av, erra := dynamodbattribute.MarshalMap(currentCondition[0])
+	if erra != nil {
+		log.Fatalf("Got error marshalling new movie item: %s", erra)
+	}
+
+	//log.Print(av)
+
+	input2 := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      av,
+	}
+
+	_, errb := svc.PutItem(input2)
+	if errb != nil {
+		log.Fatalf("Got error calling PutItem: %s", errb)
+	}
+
 }
